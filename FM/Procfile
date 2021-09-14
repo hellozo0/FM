71b7fa56f8000f417400b22e8798720179edfda4
안현주 <ahju0011@gmail.com>
2021-09-14 12:17:50 +0900

    AWS 파일 삭제

---
+++ FM/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fmproject.wsgi --log-file -
---
+++ FM/fmapp/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('community/', community, name="community"),
+    path('writef/', writef, name="writef"),
+    path('map/', map, name="map"),
+    path('developers/', developers, name="developers"),
+    path('post_search/', post_search, name="post_search"),
+]
